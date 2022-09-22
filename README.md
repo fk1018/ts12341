5a3f9f350efc3ed5ede71a6e2830ea1786f2529d
fk1018 <fk1018@users.noreply.github.com>
2022-09-22 10:25:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-oaju3z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-oaju3z)
---
+++ context/ListingsProvider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Listing } from '../components/Listing';
+import useListings from '../hooks/useListings';
+
+const ListingsContext = React.createContext(null);
+
+export function useListingsContext() {
+  return React.useContext(ListingsContext);
+}
+
+export function ListingsProvider(props) {
+  const listings = useListings();
+  if (!listings) return null;
+  return (
+    <ListingsContext.Provider value={listings}>
+      {props.children}
+    </ListingsContext.Provider>
+  );
+}
